4683d7d8b5f51a7045e5f26264417246d0d95f94
Fabroos <signorettafabrizio@gmail.com>
2024-05-09 15:12:46 -0300

    desktop callouts

--- src/app/(home)/sections/callout-1/index.tsx
+++ src/app/(home)/sections/callout-1/index.tsx
@@ -19,15 +19,18 @@ const callout = {
 export function Callout1() {
   return (
     <Section>
-      <article className="relative flex flex-col items-center justify-center gap-9 self-stretch rounded-xl border border-border bg-surface-secondary p-6 dark:border-dark-border dark:bg-dark-surface-secondary">
+      <article className="relative flex flex-col items-center justify-center gap-9 self-stretch overflow-hidden rounded-xl border border-border bg-surface-secondary p-6 dark:border-dark-border dark:bg-dark-surface-secondary">
+        {/* Lines and bg - TODO animation of rays */}
         <div className="absolute left-0 top-10 h-px w-full bg-border dark:bg-dark-border" />
         <div className="absolute bottom-[72px] left-0 h-px w-full bg-border dark:bg-dark-border" />
+        <div className="absolute bottom-7 left-[calc(100%-20vw)] h-px w-full bg-border dark:bg-dark-border" />
         <div className="absolute left-0 top-0 h-full w-full bg-surface-secondary blur-3xl dark:bg-dark-surface-secondary" />
+        {/* -------- */}
         <div className="relative z-10 flex flex-col items-center gap-2 text-center">
-          <h4 className="text-center text-3xl font-medium tracking-tighter text-text-primary dark:text-text-secondary sm:max-w-full sm:px-0">
+          <h4 className="text-center text-3xl font-medium tracking-tighter text-text-primary dark:text-text-secondary sm:max-w-full sm:px-0 md:text-4xl">
             {callout.title}
           </h4>
-          <p className="text-lg text-text-secondary dark:text-dark-text-secondary">
+          <p className="text-lg text-text-secondary dark:text-dark-text-secondary md:text-xl">
             {callout.subtitle}
           </p>
         </div>
--- src/app/(home)/sections/callout-2/index.tsx
+++ src/app/(home)/sections/callout-2/index.tsx
@@ -19,16 +19,16 @@ const callout = {
 export function Callout2() {
   return (
     <Section>
-      <article className="flex flex-col justify-center gap-9 self-stretch rounded-xl bg-neutral-100 p-6">
+      <article className="flex flex-col justify-center gap-9 self-stretch rounded-xl bg-neutral-100 p-6 lg:flex-row lg:justify-between lg:p-10">
         <div className="flex flex-col gap-2">
-          <h4 className="text-3xl font-medium text-text-primary dark:text-text-secondary">
+          <h4 className="text-3xl font-medium text-text-primary dark:text-text-secondary lg:text-4xl">
             {callout.title}
           </h4>
-          <p className="text-lg text-text-secondary dark:text-dark-text-secondary">
+          <p className="text-lg text-text-secondary dark:text-dark-text-secondary lg:text-xl">
             {callout.subtitle}
           </p>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 lg:flex-col-reverse">
           <ButtonLink href="/" intent="secondary">
             {callout.cta._title}
           </ButtonLink>
