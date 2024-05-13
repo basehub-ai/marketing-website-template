2168f2e644e61958814140b4eb3cdc6168637127
Fabroos <signorettafabrizio@gmail.com>
2024-05-13 18:06:31 -0300

    feat: Add animated lines to callout section

--- src/app/(home)/sections/callout-1/index.tsx
+++ src/app/(home)/sections/callout-1/index.tsx
@@ -1,6 +1,9 @@
+import {cx} from "class-variance-authority";
+
 import {ButtonLink} from "@/common/button";
 import {Section} from "@/common/layout";
 
+import s from "./callout-1.module.scss";
 const callout = {
   title: "AI for Every Business",
   subtitle: "Empower your organization with our AI-driven solutions.",
@@ -21,12 +24,27 @@ export function Callout1() {
     <Section>
       <article className="relative flex flex-col items-center justify-center gap-9 self-stretch overflow-hidden rounded-xl border border-border bg-surface-secondary p-6 dark:border-dark-border dark:bg-dark-surface-secondary">
         {/* Lines and bg - TODO animation of rays */}
-        <div className="absolute left-0 top-10 h-px w-full bg-border dark:bg-dark-border" />
-        <div className="absolute bottom-[72px] left-0 h-px w-full bg-border dark:bg-dark-border" />
-        <div className="absolute bottom-7 left-[calc(100%-20vw)] h-px w-full bg-border dark:bg-dark-border" />
-        <div className="absolute left-0 top-0 h-full w-full bg-surface-secondary blur-3xl dark:bg-dark-surface-secondary" />
+        <div
+          className={cx(
+            "absolute left-0 top-10 h-px w-full bg-gradient-to-l from-black/40 to-transparent dark:from-white/40 dark:to-transparent",
+            s.line,
+          )}
+        />
+        <div
+          className={cx(
+            "absolute bottom-[72px] left-0 h-px w-full bg-gradient-to-l from-black/40 to-transparent dark:from-white/40 dark:to-transparent",
+            s.line,
+          )}
+        />
+        <div
+          className={cx(
+            "absolute bottom-7 left-0 h-px w-full bg-gradient-to-l from-black/40 to-transparent dark:from-white/40 dark:to-transparent",
+            s.line,
+          )}
+        />
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-surface-secondary blur-3xl filter dark:bg-dark-surface-secondary" />
         {/* -------- */}
-        <div className="relative z-10 flex flex-col items-center gap-2 text-center">
+        <div className="relative z-20 flex flex-col items-center gap-2 text-center">
           <h4 className="text-center text-3xl font-medium tracking-tighter text-text-primary dark:text-dark-text-primary sm:max-w-full sm:px-0 md:text-4xl">
             {callout.title}
           </h4>
