0ff5d4bbbd9bc4ef33676d41352699cc73172bec
Joaquín R. Montes <joaco.rm512@gmail.com>
2024-05-31 17:10:20 -0300

    [Search]: Authors custom matcher (#4)
    * authors search
    
    * colors

--- src/common/search/index.tsx
+++ src/common/search/index.tsx
@@ -6,7 +6,12 @@ import NextLink from "next/link";
 import { cx } from "class-variance-authority";
 import * as Popover from "@radix-ui/react-popover";
 
+import { useSearchHits } from "@/context/search-hits-context";
+import { type AuthorFragment } from "@/lib/basehub/fragments";
 import { getArticleSlugFromSlugPath } from "@/lib/basehub/utils";
+
+import { Avatar } from "../avatar";
+import { AvatarsGroup } from "../avatars-group";
 
 export function SearchContent({ _searchKey }: { _searchKey: string }) {
   const search = useSearch({
@@ -46,8 +51,8 @@ export function SearchContent({ _searchKey }: { _searchKey: string }) {
           {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
           <label
             className={cx(
-              "ml-auto flex w-full items-center gap-x-1 rounded-full border border-border px-3.5 py-2.5 focus-within:!border-neutral-500 dark:border-dark-border md:max-w-[280px]",
-              search.query || "text-border dark:text-dark-border",
+              "ml-auto flex  w-full items-center gap-x-1 rounded-full border border-border px-3.5 py-2.5 focus-within:!border-neutral-500 dark:border-dark-border md:max-w-[280px]",
+              search.query || "text-text-tertiary opacity-70 dark:text-dark-text-tertiary",
             )}
           >
             <MagnifyingGlassIcon
@@ -81,17 +86,19 @@ export function SearchContent({ _searchKey }: { _searchKey: string }) {
               e.preventDefault();
             }}
           >
-            <div className="relative mx-5 min-h-20 w-[calc(100vw_-_2.5rem)] overflow-y-auto overscroll-y-contain rounded-xl border border-surface-tertiary bg-surface-primary p-2 shadow-md dark:border-dark-surface-tertiary dark:bg-dark-surface-primary md:mx-0 md:max-h-[290px] md:w-[550px]">
+            <div className="relative mx-5 min-h-20 w-[calc(100vw_-_2.5rem)] space-y-2 overflow-y-auto overscroll-y-contain rounded-xl border border-surface-tertiary bg-surface-primary p-2 shadow-md dark:border-dark-surface-tertiary dark:bg-dark-surface-primary md:mx-0 md:max-h-[320px] md:w-[550px]">
               <SearchBox.Empty asChild>
                 <div className="absolute left-1/2 top-1/2 w-fit -translate-x-1/2 -translate-y-1/2 items-center px-2 py-1 text-dark-text-tertiary">
                   No results for <span className="font-bold">&ldquo;{search.query}&rdquo;</span>
                 </div>
               </SearchBox.Empty>
 
-              <SearchBox.Placeholder>
-                <div className="flex h-[64px] animate-pulse flex-col gap-y-0.5 rounded-md border border-transparent bg-surface-tertiary px-4 dark:bg-dark-surface-secondary" />
-                <div className="mt-3 flex h-[64px] animate-pulse flex-col gap-y-0.5 rounded-md border border-transparent bg-surface-tertiary px-4 dark:bg-dark-surface-secondary" />
-                <div className="mt-3 flex h-[64px] animate-pulse flex-col gap-y-0.5 rounded-md border border-transparent bg-surface-tertiary px-4 dark:bg-dark-surface-secondary" />
+              <SearchBox.Placeholder asChild>
+                <>
+                  <div className="box-content h-[88px] animate-pulse rounded-md bg-surface-tertiary px-4 py-3 dark:bg-dark-surface-secondary" />
+                  <div className="box-content h-[88px] animate-pulse rounded-md bg-surface-tertiary px-4 py-3 dark:bg-dark-surface-secondary" />
+                  <div className="box-content h-[88px] animate-pulse rounded-md bg-surface-tertiary px-4 py-3 dark:bg-dark-surface-secondary" />
+                </>
               </SearchBox.Placeholder>
 
               <HitList hits={search.result?.hits ?? []} />
@@ -105,77 +112,84 @@ export function SearchContent({ _searchKey }: { _searchKey: string }) {
 
 function HitList({ hits }: { hits: Hit[] }) {
   return (
-    <SearchBox.HitList>
-      {hits.map((hit) => {
-        let pathname = getArticleSlugFromSlugPath(hit.document._slugPath ?? "");
-        const bodyHighlight = hit._getFieldHighlight("body");
+    <SearchBox.HitList asChild>
+      <>
+        {hits.map((hit) => {
+          let pathname = getArticleSlugFromSlugPath(hit.document._slugPath ?? "");
+          const bodyHighlight = hit._getFieldHighlight("body");
 
-        if (
-          bodyHighlight?.highlightedField?._type === "rich-text-section" &&
-          bodyHighlight.highlightedField._id
-        ) {
-          pathname += `#${bodyHighlight.highlightedField._id}`;
-        }
+          if (
+            bodyHighlight?.highlightedField?._type === "rich-text-section" &&
+            bodyHighlight.highlightedField._id
+          ) {
+            pathname += `#${bodyHighlight.highlightedField._id}`;
+          }
 
-        return (
-          <div key={hit._key} className="relative w-full last:mb-2">
-            <SearchBox.HitItem asChild hit={hit} href={pathname}>
-              <NextLink
-                className={cx(
-                  "grid grid-cols-[1fr_auto] grid-rows-[auto_1fr_auto] gap-x-1 gap-y-0.5 rounded-md border border-transparent px-4 py-3 transition-colors",
-                  "data-[selected='true']:border-surface-tertiary data-[selected='true']:bg-surface-secondary",
-                  "dark:data-[selected='true']:border-dark-surface-tertiary dark:data-[selected='true']:bg-dark-surface-secondary",
-                  "[&_mark]:bg-transparent [&_mark]:text-neutral-500",
-                )}
-                href={pathname}
-              >
-                <SearchBox.HitSnippet
-                  components={{
-                    container: HitTitleContainer,
-                  }}
-                  fieldPath="_title"
-                />
-                <SearchBox.HitSnippet
-                  components={{
-                    container: HitBodyContainer,
-                  }}
-                  fallbackFieldPaths={["introduction"]}
-                  fieldPath="body"
-                />
-                <SearchBox.HitSnippet
-                  components={{
-                    container: HitAvatarContainer,
-                    // text: ({ children }) => {
-                    //   return <span>children</span>;
-                    // },
-                  }}
-                  // fallbackFieldPaths={[
-                  //   "authors.1._title",
-                  //   "authors.2._title",
-                  //   "authors.3._title",
-                  //   "authors.4._title",
-                  //   "authors.5._title",
-                  // ]}
-                  fieldPath="authors"
-                />
-                <SearchBox.HitSnippet
-                  components={{
-                    container: HitCategoryContainer,
-                  }}
-                  fieldPath="categories"
-                />
-              </NextLink>
-            </SearchBox.HitItem>
-          </div>
-        );
-      })}
+          const field = hit._getField("authors");
+          let firstHighlightedAuthorId: string | undefined = undefined;
+
+          for (const h of hit.highlights) {
+            if (h.fieldPath.startsWith("authors")) {
+              const index = h.fieldPath.split(".")[1];
+              const id = hit._getField(`authors.${index}._id`);
+
+              if (typeof id === "string") {
+                firstHighlightedAuthorId = id;
+              }
+              break;
+            }
+          }
+
+          return (
+            <div key={hit._key} className="relative w-full last:mb-2">
+              <SearchBox.HitItem asChild hit={hit} href={pathname}>
+                <NextLink
+                  className={cx(
+                    "flex grid-rows-[auto_1fr_auto] flex-col gap-y-0.5 rounded-md px-4 py-3 transition-colors",
+                    "data-[selected='true']:bg-surface-tertiary",
+                    "dark:data-[selected='true']:bg-dark-surface-tertiary",
+                    "[&_mark]:bg-transparent [&_mark]:text-neutral-500",
+                  )}
+                  href={pathname}
+                >
+                  <SearchBox.HitSnippet
+                    components={{
+                      container: HitTitleContainer,
+                    }}
+                    fieldPath="_title"
+                  />
+                  <SearchBox.HitSnippet
+                    components={{
+                      container: HitBodyContainer,
+                    }}
+                    fallbackFieldPaths={["introduction"]}
+                    fieldPath="body"
+                  />
+                  <div className="mt-3 flex justify-between gap-x-1">
+                    <CustomAvatarHit
+                      authors={field as AuthorFragment[]}
+                      match={firstHighlightedAuthorId}
+                    />
+                    <SearchBox.HitSnippet
+                      components={{
+                        container: HitContainer,
+                      }}
+                      fieldPath="categories"
+                    />
+                  </div>
+                </NextLink>
+              </SearchBox.HitItem>
+            </div>
+          );
+        })}
+      </>
     </SearchBox.HitList>
   );
 }
 
 function HitTitleContainer({ children }: React.PropsWithChildren) {
   return (
-    <p className="col-span-full truncate leading-normal text-text-primary dark:text-dark-text-primary">
+    <p className="truncate leading-normal text-text-primary dark:text-dark-text-primary">
       {children}
     </p>
   );
@@ -183,27 +197,44 @@ function HitTitleContainer({ children }: React.PropsWithChildren) {
 
 function HitBodyContainer({ children }: React.PropsWithChildren) {
   return (
-    <p className="col-span-full line-clamp-2 text-sm text-text-secondary dark:text-dark-text-secondary">
-      {children}
-    </p>
+    <p className="truncate text-sm text-text-tertiary dark:text-dark-text-tertiary">{children}</p>
   );
 }
 
-const getNodeText = (node: React.ReactNode): string | number => {
-  if (["string", "number"].includes(typeof node)) return node as string | number;
-  if (node instanceof Array) return node.map(getNodeText).join("");
+function CustomAvatarHit({
+  match,
+  authors,
+}: {
+  match: string | undefined;
+  authors: { _title: string; _id: string }[];
+}) {
+  const { authorsAvatars } = useSearchHits();
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-  if (typeof node === "object" && node && "props" in node) return getNodeText(node.props.children);
-  throw new Error(`Unsupported node type: ${typeof node}`);
-};
+  if (match) {
+    return (
+      <div className="flex items-center gap-x-1.5">
+        <Avatar {...authorsAvatars[match]} />
+        <SearchBox.HitSnippet
+          components={{
+            container: HitContainer,
+          }}
+          fieldPath="authors"
+        />
+      </div>
+    );
+  }
 
-function HitAvatarContainer({ children }: React.PropsWithChildren) {
-  console.log(getNodeText(children));
+  return (
+    <AvatarsGroup>
+      {authors.map((author) => {
+        const avatar = authorsAvatars[author._id];
 
-  return <p className="text-sm text-text-tertiary dark:text-dark-text-tertiary">{children}</p>;
+        return <Avatar key={author._id} {...avatar} alt={author._title} />;
+      })}
+    </AvatarsGroup>
+  );
 }
 
-function HitCategoryContainer({ children }: React.PropsWithChildren) {
-  return <p className="text-sm text-text-tertiary dark:text-dark-text-tertiary">{children}</p>;
+function HitContainer({ children }: React.PropsWithChildren) {
+  return <p className="text-sm text-text-secondary dark:text-dark-text-secondary">{children}</p>;
 }
