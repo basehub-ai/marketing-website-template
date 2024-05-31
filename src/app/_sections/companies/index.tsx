cb771ed5485b24c0ac7217080bc9e3be12b7cf09
Julian Benegas <julianbenegas99@gmail.com>
2024-05-31 15:09:10 -0300

    subtitle

--- src/app/_sections/companies/index.tsx
+++ src/app/_sections/companies/index.tsx
@@ -7,6 +7,7 @@ import { fragmentOn } from ".basehub/schema";
 import s from "./companies.module.scss";
 
 export const companiesFragment = fragmentOn("CompaniesComponent", {
+  subtitle: true,
   companies: {
     items: {
       _title: true,
@@ -23,9 +24,7 @@ type Companies = fragmentOn.infer<typeof companiesFragment>;
 export function Companies(props: Companies) {
   return (
     <Section container="full">
-      <h2 className="text-center text-dark-text-tertiary opacity-50">
-        Join 4,000 companies already growing
-      </h2>
+      <h2 className="text-center text-dark-text-tertiary opacity-50">{props.subtitle}</h2>
       <div className="no-scrollbar flex max-w-full justify-center overflow-auto">
         <div className="pointer-events-none absolute left-0 top-0 h-full w-[30vw] bg-transparent bg-gradient-to-r from-surface-primary dark:from-dark-surface-primary xl:hidden" />
         <div className="pointer-events-none absolute right-0 top-0 h-full w-[30vw] bg-transparent bg-gradient-to-l from-surface-primary dark:from-dark-surface-primary xl:hidden" />
