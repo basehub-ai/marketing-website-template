7893af508fff4f3fabff5d02713924dd78e4ea11
Fabroos <signorettafabrizio@gmail.com>
2024-06-04 14:51:34 -0300

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body
-        className={`min-h-svh bg-surface-primary text-text-primary dark:bg-dark-surface-primary dark:text-dark-text-primary ${GeistMono.variable} ${GeistSans.variable} overflow-x-clip font-sans`}
+        className={`min-h-svh max-w-[100vw] bg-surface-primary text-text-primary dark:bg-dark-surface-primary dark:text-dark-text-primary ${GeistMono.variable} ${GeistSans.variable} overflow-x-clip font-sans`}
       >
         <Providers>
           <Header />
--- src/common/layout.tsx
+++ src/common/layout.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-export const $section = cva("py-14 md:py-[72px] flex flex-col items-center gap-10 relative max-w-[100vw]", {
+export const $section = cva("py-14 md:py-[72px] flex flex-col items-center gap-10 relative", {
   variants: {
     container: {
       default: "container mx-auto px-6",
