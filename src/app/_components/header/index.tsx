3dbb192fc51b19f30d125a9b54be0b255fa464bd
Fabroos <signorettafabrizio@gmail.com>
2024-06-21 10:51:14 -0300

    fix header logo

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -89,8 +89,8 @@ export async function Header() {
           <header className="sticky left-0 top-0 z-[100] flex w-full flex-col border-b border-border bg-surface-primary dark:border-dark-border dark:bg-dark-surface-primary">
             <div className="flex h-[--header-height] bg-surface-primary dark:bg-dark-surface-primary">
               <div className="container mx-auto grid w-full grid-cols-header place-items-center content-center items-center px-6 first:*:justify-self-start">
-                <ButtonLink unstyled className="ring-offset-2" href="/">
-                  <DarkLightImage priority alt="logo" className="h-10 " {...settings.logo} />
+                <ButtonLink unstyled className="flex items-center ring-offset-2" href="/">
+                  <DarkLightImage priority alt="logo" className="h-5 w-auto" {...settings.logo} />
                 </ButtonLink>
                 <DesktopMenu {...header} />
                 <MobileMenu {...header} />
