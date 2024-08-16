3533e3d1dadf80d590c3f0ff1287137bdcc44e18
Julian Benegas <julianbenegas99@gmail.com>
2024-08-16 10:50:32 -0300

    dont hardcode back to changelog text

--- src/app/_sections/newsletter/newsletter.tsx
+++ src/app/_sections/newsletter/newsletter.tsx
@@ -1,33 +1,58 @@
-"use client";
-import React from "react";
+import * as React from "react";
 
 import { Input } from "@/common/input";
 import { Section } from "@/common/layout";
+import { Pump } from ".basehub/react-pump";
+import { draftMode } from "next/headers";
+import { BASEHUB_REVALIDATE_TIME } from "@/lib/basehub/constants";
 
 export function Newsletter() {
   return (
-    <Section
-      className="bg-surface-secondary !py-10 dark:bg-dark-surface-secondary"
-      container="full"
+    <Pump
+      draft={draftMode().isEnabled}
+      next={{ revalidate: BASEHUB_REVALIDATE_TIME }}
+      queries={[
+        {
+          site: {
+            footer: {
+              newsletter: {
+                title: true,
+                description: true,
+              },
+            },
+          },
+        },
+      ]}
     >
-      <div className="container mx-auto flex flex-col gap-4 px-6 lg:flex-row lg:justify-between">
-        <div className="flex flex-col items-start gap-1">
-          <h5 className="text-xl font-medium lg:text-2xl">Stay Ahead of the AI Curve</h5>
-          <p className="text text-text-tertiary dark:text-dark-text-tertiary lg:text-lg">
-            Join our newsletter for exclusive insights and updates on the latest AI trends.
-          </p>
-        </div>
+      {async ([{ site }]) => {
+        "use server";
 
-        <form className="w-full max-w-[400px] flex-1">
-          <Input
-            required
-            buttonContent="Subscribe"
-            name="email"
-            placeholder="Enter your email"
-            type="email"
-          />
-        </form>
-      </div>
-    </Section>
+        return (
+          <Section
+            className="bg-surface-secondary !py-10 dark:bg-dark-surface-secondary"
+            container="full"
+          >
+            <div className="container mx-auto flex flex-col gap-4 px-6 lg:flex-row lg:justify-between">
+              <div className="flex flex-1 flex-col items-start gap-1">
+                <h5 className="text-xl font-medium lg:text-2xl">{site.footer.newsletter.title}</h5>
+                <p className="text text-text-tertiary dark:text-dark-text-tertiary lg:text-lg">
+                  {site.footer.newsletter.description}
+                </p>
+              </div>
+
+              <form className="w-full max-w-[400px] flex-shrink-0">
+                <Input
+                  required
+                  buttonContent="Subscribe"
+                  name="email"
+                  placeholder="Enter your email"
+                  type="email"
+                />
+              </form>
+            </div>
+          </Section>
+        );
+      }}
+    </Pump>
   );
 }
