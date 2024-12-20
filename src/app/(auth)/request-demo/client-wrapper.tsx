81769edaf4f97b559ccf90274aa09b828d0836a7
Joaquin Montes <joaco.rm512@gmail.com>
2024-12-20 11:58:15 -0300

    wip

--- src/app/(auth)/request-demo/client-wrapper.tsx
+++ src/app/(auth)/request-demo/client-wrapper.tsx
@@ -1,8 +1,10 @@
 "use client";
+import * as React from "react";
+import { sendEvent } from "basehub/events";
 import { LabeledInput } from "../_components/labeled-input";
-import { unstable_Form as Form } from "basehub/react-form";
 import { Field } from "basehub/react-form";
 import { LabeledTextarea } from "../_components/labeled-input";
+import { Submissions } from ".basehub/schema";
 
 export function DemoForm({
   children,
@@ -10,28 +12,41 @@ export function DemoForm({
   schema,
 }: {
   children: React.ReactNode;
-  ingestKey: string;
+  ingestKey: Submissions["ingestKey"];
   schema: Field[];
 }) {
+  const handleSubmit = React.useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const form = e.target as HTMLFormElement;
+      const formData = new FormData(form);
+
+      await sendEvent(ingestKey, {
+        eMail: String(formData.get("email")),
+        firstName: String(formData.get("firstName")),
+        lastName: String(formData.get("lastName")),
+        message: String(formData.get("message")),
+      });
+    },
+    [ingestKey],
+  );
+
   return (
-    <Form
-      className="flex flex-col gap-3"
-      schema={schema}
-      action={{
-        type: "send",
-        ingestKey,
-      }}
-      components={{
-        text: (props) =>
-          props.name === "message" ? (
-            <LabeledTextarea {...props} minLength={16} rows={8} className="max-h-64 min-h-16" />
-          ) : (
-            <LabeledInput {...props} />
-          ),
-        email: (props) => <LabeledInput {...props} />,
-      }}
-    >
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      {schema.map((field) =>
+        field.name === "message" ? (
+          <LabeledTextarea
+            key={field.id}
+            minLength={16}
+            rows={8}
+            className="max-h-64 min-h-16"
+            {...field}
+          />
+        ) : (
+          <LabeledInput key={field.id} {...field} />
+        ),
+      )}
       {children}
-    </Form>
+    </form>
   );
 }
